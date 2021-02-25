956973918bdbf5dd7632e8ba4b93bf3cfa57e4cd
Hayden Garrard <hagarrard@gmail.com>
2021-02-25 17:27:56 -0600

    first commit

---
+++ 02-Homework/Develop/index.js
@@ -0,0 +1,5 @@
+// questions for employee 
+                    //.then >> create employee object 
+                            // >> type of employee :
+                                         // if manager >> create Manger Object
+                                        //  else if intern >> create Intern Object
---
+++ 02-Homework/Develop/lib/Employee.js
@@ -0,0 +1,25 @@
+class Employee{
+
+        constructor(name,id,email){
+            this.name = name;
+            this.id = id;
+            this.email = email;
+        }
+
+        getName (){
+            return this.name;
+        }
+        getId (){
+            return this.id;
+        }
+        getEmail (){
+            return this.email;
+        }
+        getRole (){
+            return "Employee";
+        }
+}
+
+// var testOB = new Employee("test",1, "test@test.com")
+// console.log(testOB);
+module.exports = Employee;
