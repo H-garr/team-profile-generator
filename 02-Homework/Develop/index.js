eeb1bc76f8d2df11c3f1fba157b78a0d9282c76e
Hayden Garrard <hagarrard@gmail.com>
2021-03-04 19:57:35 -0600

    added menu

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -4,15 +4,35 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const template = require("./src/page-template")
+const template = require("./src/page-template");
+const e = require("express");
 var wholeteam = [];
 var teamidarray = [];
 // had help creating the two variables below and some of the requires i was missing
 var newdirectory = path.resolve(_dirname, "output")
 var newpath = path.join(newdirectory, "team.html")
 
-function buildTeam() {
-        // we will create all of our teams from here using external inputs
+function startMenu() {
+        inquirer.prompt([{
+                type:"list",
+                name:"startmenu",
+                message:"Choose one of the following options of what you would like to do.",
+                choices:["Start building your team","View current team","Exit"]}
+        ]).then((e) => {
+                switch(e.startMenu){
+                        case "Start building your team":
+                                buildManagers();
+                                break;
+                        case "View current team":
+                                // open browser window here
+                                break;
+                        case "Exit":
+                                console.log("Please press 'control' and 'C' to exit,See you next time!")
+
+                }
+        });
+        }
+        // we will create all of our teams from here using external inputs from user
         function buildManagers() {
                 inquirer.prompt([
                                 {
@@ -75,9 +95,10 @@ function buildTeam() {
         teamidarray.push(response.managerID)
         // pushing the ID into the id array and the manager into the team array
         wholeteam.push(manager);
-
+        // then we call to build the team next here
+        buildTeam();
 })
         }
-}
+
 
 
