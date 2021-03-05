b1aeaf25141b50754984155744db9bea851d8349
Hayden Garrard <hagarrard@gmail.com>
2021-03-04 18:58:55 -0600

    manager portion done

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -6,16 +6,15 @@ const path = require("path");
 const fs = require("fs");
 const template = require("./src/page-template")
 var wholeteam = [];
-var teamidarr = [];
-// had help creating the two variables below
+var teamidarray = [];
+// had help creating the two variables below and some of the requires i was missing
 var newdirectory = path.resolve(_dirname, "output")
 var newpath = path.join(newdirectory, "team.html")
 
 function buildTeam() {
         // we will create all of our teams from here using external inputs
         function buildManagers() {
-                inquirer.prompt(
-                        [
+                inquirer.prompt([
                                 {
                                         type: "input",
                                         name: "managerName",
@@ -33,46 +32,51 @@ function buildTeam() {
                                 },
                                 {
                                         type: "input",
-                                        name: "managerId",
+                                        name: "managerID",
                                         message: "Please enter in manager's ID number.",
                                         validate: response => {
                                                 var token = response.match(/^[1-9]\d*$/);
-//learned about regex here and this allows the id to be inputed as a number.
-// also wrote in es6 and es5 to show i am able to do both. 
-                                                                if (token) {
-                                                                        return true;
-                                                                }
-                                                                else return "That does not seem to be a valid response please try again. Make sure you have entered in the correct ID. ";
-                                                        }
-},{
-                                        type:"input",
-                                        name:"managerEmail",
-                                        message:"Please enter in your manager's Email.",
-                                        validate: response => {
-                                                const token = response.match(/\S+@\S+\.\S+/);
-                                                // regex for string,@ sign string then . string
-                                        
-                                        if (response) {
-                                                return true;
-                                        }
-                                        else return "That does not seem to be a valid response. Please make sure your email is formatted like this : test@test.com";
-                                }
-},{
-                                        type:"input",
-                                        name:"managerOffice",
-                                        message:"Please enter in your manager's office number.",
-                                        validate: response => {
-                                                var token = response.match( /^[1-9]\d*$/);
+                                                //learned about regex here and this allows the id to be inputed as a number and verified that it is a number.
+                                                // also wrote in es6 and es5 to show i am able to do both. 
                                                 if (token) {
                                                         return true;
                                                 }
-                                                else return "That office number does not seem to match our criteria. Please make sure you are entering in the number correctly.";
+                                                else return "That does not seem to be a valid response please try again. Make sure you have entered in the correct ID. ";
                                         }
-}
+                                }, {
+                                        type: "input",
+                                        name: "managerEmail",
+                                        message: "Please enter in your manager's Email.",
+                                        validate: response => {
+                                                var token = response.match(/\S+@\S+\.\S+/);
+                                                // regex for string,@ sign string then . string
 
+                                                if (response) {
+                                                        return true;
+                                                }
+                                                else return "That does not seem to be a valid response. Please make sure your email is formatted like this : test@test.com";
+                                        }
+                                }, {
+                                        type: "input",
+                                        name: "managerOffice",
+                                        message: "Please enter in your manager's office number.",
+                                        validate: response => {
+                                                var token = response.match(/^[1-9]\d*$/);
+                                                if (token) {
+                                                        return true;
+                                                }
+                                                else return "That office number does not seem to match the criteria. Please make sure you are entering in the number correctly.";
+                                        }
+                                }
+                        ])
+.then(response => {
+        let manager = new Manager(response.managerName,response.managerID,response.managerEmail,response.managerOffice);
+        // re-writing manager here
+        teamidarray.push(response.managerID)
+        // pushing the ID into the id array and the manager into the team array
+        wholeteam.push(manager);
 
-                        ]
-                )
+})
         }
 }
 
